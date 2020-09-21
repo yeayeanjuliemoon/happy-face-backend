0f0b86c81aa96ade8881d3518f6600f9518d1a3d
Julie Moon <juliemoon95@gmail.com>
2020-09-20 21:35:17 -0400

    initial commit

---
+++ src/main/java/com/skincare/happyfacebackend/HappyFaceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.skincare.happyfacebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappyFaceBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HappyFaceBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/skincare/happyfacebackend/HappyFaceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skincare.happyfacebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HappyFaceBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
